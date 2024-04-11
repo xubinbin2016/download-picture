ac8c94d5007399191f8269eff558acc3abaa5395
xubinbin <843087898@qq.com>
2024-04-11 15:52:48 +0800

    使用vue写的页面，一键把所有照片打包成zip,然后下载

--- popup.js
+++ popup.js
@@ -1,40 +1,6 @@
-async function downJson(url) {
-    console.log(url);
-    const response = await fetch(url)
-
-    // text: 返回文本数据
-    response.text().then(text => {
-        // 解析json数据
-        const startIndex = text.indexOf('(');
-        const endIndex = text.lastIndexOf(')');
-        const json = JSON.parse(text.substring(startIndex + 1, endIndex));
-        console.log(json);
-    });
-
-    // blob: 返回二进制数据流，适合图片等二进制数据
-    // response.blob().then(blob => {
-    //     const url = URL.createObjectURL(blob);  
-    //     console.log(url);
-    // });
-
-    // json: 返回json数据
-    // response.json().then(json => {  
-    //     console.log(json);
-    // });
-
-    // arrayBuffer: 返回二进制数据流，适合音频等二进制数据
-    // response.arrayBuffer().then(buffer => {
-    //     console.log(buffer);
-    // });
-
-    // formData: 返回表单数据
-    // response.formData().then(data => {
-    //     console.log(data);
-    // });
-}
-
 chrome.storage.sync.get("url", ({ url }) => {
+    localStorage.setItem("qzoneurl", url);
     document.querySelector('button').onclick = () => {
-        window.open('./picture.html')
+        window.open('./dist/index.html')
     };
 });
